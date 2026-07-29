34996ef5ccb483e4ef6b2fd882515522ea4d0df5
joakim <joakim@kobler.no>
2026-07-29 19:45:33 +0200

    Initial Commit

---
+++ Ustastrikk/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ustastrikk
+//
+//  Created by Joakim Warholm on 29/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ustastrikk/UstastrikkApp.swift
@@ -0,0 +1,17 @@
+//
+//  UstastrikkApp.swift
+//  Ustastrikk
+//
+//  Created by Joakim Warholm on 29/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct UstastrikkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
